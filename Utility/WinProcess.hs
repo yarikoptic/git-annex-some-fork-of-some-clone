ca0aaaf5ab4f49c17e71bfce7cac28b6bb03ee0e
Joey Hess <joey@kitenet.net>
2014-02-27 17:10:14 -0400

    hask

--- Utility/WinProcess.hs
+++ Utility/WinProcess.hs
@@ -6,10 +6,16 @@
  -}
 
 {-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE CPP #-}
 
 module Utility.WinProcess where
 
 import Utility.PID
 
+#ifndef WITH_WINSPLICE
 foreign import ccall unsafe "terminatepid"
 	terminatePID :: PID -> IO ()
+#else
+terminatePID :: PID -> IO ()
+terminatePID _ = return ()
+#endif
